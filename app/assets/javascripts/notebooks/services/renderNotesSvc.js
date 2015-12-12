6c1d4cfc8f76e79788b8b1a82654889f0c51eb84
Eric Stensland <estensland88@gmail.com>
2015-12-11 21:00:17 -0600

    working on note view

--- app/assets/javascripts/notebooks/services/renderNotesSvc.js
+++ app/assets/javascripts/notebooks/services/renderNotesSvc.js
@@ -4,16 +4,6 @@ angular.module('notebookApp')
     function () {
       var SlapOnPage;
 
-      SlapOnPage = function(element, note){
-        var noteElement = angular.element('<div>' + note.body + '</div>')
-        noteElement.css('width', ('' + note.hstore.width + 'px'))
-        noteElement.css('position', 'absolute')
-        noteElement.offset({left: note.hstore.x, top: note.hstore.y})
-        element.append(noteElement)
-      }
 
-      return {
-        slapOnPage: SlapOnPage
-      }
 
   }]);
