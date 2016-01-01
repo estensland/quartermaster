a37f6f379c8b70aedfa0372e0e819744b1575b8c
Eric Stensland <estensland88@gmail.com>
2016-01-01 16:31:07 -0600

    index for binders

--- app/serializers/binder_serializer.rb
+++ app/serializers/binder_serializer.rb
@@ -1,6 +1,7 @@
 class BinderSerializer < ActiveModel::Serializer
   attributes  :id,
               :name,
+              :description,
               :color_hex,
               :created_at,
               :updated_at,
