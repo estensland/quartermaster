4967bd93ceffbbc431bfec04fd738b2dcc7f7839
Eric Stensland <estensland88@gmail.com>
2016-01-17 14:10:12 -0600

    adding puma

---
+++ config/puma.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env puma
+
+environment ENV['RAILS_ENV'] || 'production'
+
+daemonize true
+
+pidfile "/var/www/quartermaster/shared/tmp/pids/puma.pid"
+stdout_redirect "/var/www/quartermaster/shared/tmp/log/stdout", "/var/www/quartermaster/shared/tmp/log/stderr"
+
+threads 0, 16
+
+bind "unix:///var/www/quartermaster/shared/tmp/sockets/puma.sock"
